d80846412f236d6ff543f069ecc16dcfc6100c38
zhaoyajie <878182159@qq.com>
2019-09-06 18:13:20 +0800

    开个头，加油。

---
+++ src/pages/index/app.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>3.0管理平台技术验证</h1>
+    <h2>登录页面</h2>
+    <el-button type="primary" @click="login">登录</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Use Vue 2.0 Today!'
+    }
+  },
+  methods: {
+    login(){
+      location.assign('manager.html')
+    }
+  }
+}
+</script>
+
+<style lang="postcss">
+.index-card {
+  width: 800px;
+  margin: 100px auto;
+}
+</style>
---
+++ src/pages/manager/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+        <section ></section>
+    </section>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style scoped>
+
+</style>
